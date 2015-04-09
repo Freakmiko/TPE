5a02bd09fc37a50a9f18629cf5f95450813105d0
Michael Kurila <freakmiki@gmail.com>
2015-04-09 18:26:46 +0200

    Translated comments, fixed tree selection, fixed main while loop so that you can exit the menu properly

--- src/com/tpe/gruppenummer/uebung01/BTreeMenu.java
+++ src/com/tpe/gruppenummer/uebung01/BTreeMenu.java
@@ -4,35 +4,32 @@ import static gdi.MakeItSimple.*;
 
 public class BTreeMenu {
 
-    // Array mit 9 BTrees
+    // Array with 9 BTrees
     static BTree bTreeArray[] = new BTree[9];
 
-    // Standard workingTree
     static int workingTree = 0;
 
-    @SuppressWarnings("InfiniteLoopStatement")
+    @SuppressWarnings({"StatementWithEmptyBody"})
     public static void main(String[] args) {
 
-        println("Geben Sie die Ordnung des Baumes an:");
+        println("Please enter the order of the trees:");
 
-        int ordnung = readInt();
+        int order = readInt();
         readLine();
 
         for (int i = 0; i < bTreeArray.length; i++)
-            bTreeArray[i] = new BTreeImplementation(ordnung);
+            bTreeArray[i] = new BTreeImplementation(order);
 
-        while (true) {
-            makeMenu();
-        }
+        while(makeMenu()) {}
     }
 
-    public static void makeMenu() {
+    public static boolean makeMenu() {
 
         Integer tempValue;
         String filename;
         int workingTreeNumber;
 
-        println("------------------- Menü ------------------");
+        println("------------------- Menu ------------------");
         println("01) insert(Integer object)");
         println("02) insert(String filename)");
         println("03) contains(Integer object)");
@@ -47,7 +44,7 @@ public class BTreeMenu {
         println("12) printPostorder()");
         println("13) printPreorder()");
         println("14) printLevelorder()");
-        println("15) Alle Printmethoden");
+        println("15) All Printmethods");
         println("16) change workingTree");
         println("17) Exit");
 
@@ -58,142 +55,134 @@ public class BTreeMenu {
 
             // insert(Integer object)
             case 1:
-                println("Geben Sie den einzufügenden Wert ein:");
+                println("Please enter the value to insert:");
                 tempValue = readInt();
-                if (bTreeArray[workingTree].insert(tempValue)) {
-                    println("Der Wert konnte in den aktuellen Baum eingefügt werden.");
-                } else {
-                    println("Der Wert konnte nicht in den aktuellen baum eingefügt werden.");
-                }
+                if (bTreeArray[workingTree].insert(tempValue))
+                    println("The value has been inserted into the tree.");
+                else
+                    println("The value could not be inserted (duplicate).");
 
                 break;
 
             // insert(String filename)
             case 2:
-                println("Geben Sie die Dateinamen mit den einzufügenden Werten ein:");
+                println("Please enter the path to the file:");
                 filename = readLine();
-                if (bTreeArray[workingTree].insert(filename)) {
-                    println("Alle werte aus der Datei konnten in den aktuellen Baum eingefügt werden.");
-                } else {
-                    println("Mindestens einer der Werte aus der Datei konnte nicht in den aktuellen Baum eingefügt werde.");
-                }
+                if (bTreeArray[workingTree].insert(filename))
+                    println("Every element in the file has been inserted into the tree.");
+                else
+                    println("Only some elements have been inserted into the tree because there was a duplicate.");
                 break;
 
             // contains(Integer object)
             case 3:
-                println("Geben Sie den gesuchten Wert ein:");
+                println("Please enter the value that you want to look for:");
                 tempValue = readInt();
-                if (bTreeArray[workingTree].contains(tempValue)) {
-                    println("Der gesuchte Wert konnte im aktuellen Baum gefunden werden.");
-                } else {
-                    println("Der gesuchte Wert konnte nicht im aktuellen Baum gefunden werde.");
-                }
+                if (bTreeArray[workingTree].contains(tempValue))
+                    println("The value is in the tree.");
+                else
+                    println("The value is not in the tree.");
                 break;
 
             // size()
             case 4:
-                println("Im aktuellen Baum sind " + bTreeArray[workingTree].size() + " Werte.");
+                println("The size of the tree is " + bTreeArray[workingTree].size() + ".");
                 break;
 
             // height
             case 5:
-                println("Der aktuellen Baum hat die Höhe " + bTreeArray[workingTree].height() + ".");
+                println("The height of the tree is " + bTreeArray[workingTree].height() + ".");
                 break;
 
             // getMax()
             case 6:
-                println("Im aktuellen Baum ist " + bTreeArray[workingTree].getMax() + " der größte Wert.");
+                println("The maximum of the tree is " + bTreeArray[workingTree].getMax() + ".");
                 break;
 
             // getMin()
             case 7:
-                println("Im aktuellen Baum ist " + bTreeArray[workingTree].getMin() + " der kleinste Wert.");
+                println("The minimum of the tree is " + bTreeArray[workingTree].getMin() + ".");
                 break;
 
             // isEmpty()
             case 8:
-                if (bTreeArray[workingTree].isEmpty()) {
-                    println("Der aktuelle Baum ist leer.");
-                } else {
-                    println("Der aktuelle Baum ist nicht leer.");
-                }
+                if (bTreeArray[workingTree].isEmpty())
+                    println("The tree is empty.");
+                else
+                    println("The tree is not empty.");
                 break;
 
             // addAll(BTree otherTree)
             case 9:
-                println("Geben Sie die Nummer des Baumes ein, den Sie in den aktuellen Baum einfügen möchten:");
+                println("Please enter the number of the tree from which you want all values to be inserted\ninto the current tree:");
                 bTreeArray[workingTree].addAll(bTreeArray[readInt()]);
-                println("Der gewünschte Baum wurde dem aktuellen Baum hinzugefügt.");
+                println("All values have been inserted.");
                 break;
 
             // cloneDeep(BTree otherTree)
             case 10:
-                println("Geben Sie die Nummer des Baumes ein, den Sie tief in den aktuleen Baum kopieren möchten: ");
+                println("Please enter the number of the tree that you want to be cloned deep over the current tree: ");
                 bTreeArray[workingTree] = BTreeImplementation.cloneDeep(bTreeArray[readInt()]);
-                println("Der gewünschte Baum wurde tief in den aktuellen Baum kopiert.");
+                println("The tree has been cloned.");
                 break;
 
             // printInorder()
             case 11:
-                println("Der aktuelle Baum sieht mit Inorder wie folgt aus:");
                 println();
                 bTreeArray[workingTree].printInorder();
                 break;
 
             // printPostorder()
             case 12:
-                println("Der aktuelle Baum sieht mit Postorder wie folgt aus:");
                 println();
                 bTreeArray[workingTree].printPostorder();
                 break;
 
             // printPreorder()
             case 13:
-                println("Der aktuelle Baum sieht mit Preorder wie folgt aus:");
                 println();
                 bTreeArray[workingTree].printPreorder();
                 break;
 
             // printLevelorder
             case 14:
-                println("Der aktuelle Baum sieht mit Levelorder wie folgt aus:");
                 println();
                 bTreeArray[workingTree].printLevelorder();
                 break;
 
             case 15:
-                println("Der aktuelle Baum sieht mit Inorder wie folgt aus:");
+                println("Inorder:");
                 println();
                 bTreeArray[workingTree].printInorder();
-                println("Der aktuelle Baum sieht mit Postorder wie folgt aus:");
+                println("Postorder:");
                 println();
                 bTreeArray[workingTree].printPostorder();
-                println("Der aktuelle Baum sieht mit Preorder wie folgt aus:");
+                println("Preorder:");
                 println();
                 bTreeArray[workingTree].printPreorder();
-                println("Der aktuelle Baum sieht mit Levelorder wie folgt aus:");
+                println("Levelorder:");
                 println();
                 bTreeArray[workingTree].printLevelorder();
                 break;
 
             // change workingTree
             case 16:
-                println("Geben Sie die Nummer des gewünschten Baumes ein:");
+                println("Please enter the number of the tree you want to switch to:");
                 workingTreeNumber = readInt();
-                if (workingTreeNumber < 0 && workingTreeNumber > 9) {
-                    println("Die gewünschte Nummer ist nicht möglich.");
+                if (workingTreeNumber < 0 || workingTreeNumber > 9) {
+                    println("This is not a valid number.");
                 } else {
                     workingTree = workingTreeNumber;
                 }
                 break;
             case 17:
-                return;
+                return false;
 
             default:
                 break;
 
         }
         readLine();
-
+        return true;
     }
 }
